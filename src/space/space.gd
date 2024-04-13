61f9c606efede5b0666b564c05856a7a65725479
Meptl <git-client@meptl.com>
2024-04-13 14:08:06 -0400

    Follow path

--- src/space/space.gd
+++ src/space/space.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
+@export var speed = 2.0
+@export var pre: Array[PackedScene]
+
 @onready var cam = $Camera2D
+@onready var icon = $Icon
 
 var dragging = false
 var drag_start = Vector2.ZERO
+
+var hist = []
+
+# func _ready():
+# 	follow_reagents(pre)
 
 
 func _input(event):
@@ -20,3 +29,22 @@ func _input(event):
 			var drag_distance = drag_start - drag_end
 			cam.position += drag_distance
 			drag_start = drag_end
+
+
+func follow_reagents(reagents):
+	hist.append(icon.position)
+	for r in reagents:
+		var info = r.instantiate()
+		add_child(info)
+
+		var start_pos = icon.position
+		for point in info.path.get_baked_points():
+			icon.position = start_pos + point
+			hist.append(icon.position)
+			queue_redraw()
+			await get_tree().physics_frame
+
+
+func _draw():
+	for i in range(1, hist.size()):
+		draw_line(hist[i - 1], hist[i], Color.WHITE, 2)
--- src/ui/ui.gd
+++ src/ui/ui.gd
@@ -2,8 +2,13 @@ extends Control
 
 @onready var reagent_table = %ReagentTable
 @onready var summon_pool = %SummonPool
+@onready var space = %Space
 
 
 func _ready():
 	for child in reagent_table.get_children():
 		child.selected.connect(summon_pool.add_reagent)
+
+
+func _on_CookButton_pressed():
+	space.follow_reagents(summon_pool.reagents)
