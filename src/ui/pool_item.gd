bce8eaf1ec4aedd8b0491e80531a8afb12e8dfbb
Meptl <git-client@meptl.com>
2024-04-14 14:44:07 -0400

    Enable uncharging

--- src/ui/pool_item.gd
+++ src/ui/pool_item.gd
@@ -10,6 +10,7 @@ signal remove_requested(src)
 var charge_max = 1.2
 var charge = 0.0
 var charging = false
+var uncharging = false
 
 
 func amount():
@@ -21,9 +22,13 @@ func _process(_delta):
 
 
 func _physics_process(delta):
-	if not charging:
+	if not charging and not uncharging:
 		return
-	charge += delta
+	if charging:
+		charge += delta
+	elif uncharging:
+		charge -= delta
+	charge = clampf(charge, 0.0, 1.0)
 	# 1.0 is darkened. 0.0 is done.
 	darken.material.set_shader_parameter(
 		"EdgePosition", clampf((charge_max - charge) / charge_max, 0.0, 1.0)
@@ -34,14 +39,6 @@ func set_icon(i):
 	icon_rect.texture = i
 
 
-func _on_button_down():
-	charging = true
-
-
-func _on_button_up():
-	charging = false
-
-
 func _on_gui_input(event):
 	if event is InputEventMouseButton:
 		match event.button_index:
@@ -49,5 +46,8 @@ func _on_gui_input(event):
 				charging = event.pressed
 				if charging:
 					charge_start.emit()
+				print(charging)
 			MOUSE_BUTTON_RIGHT:
-				remove_requested.emit(self)
+				if event.pressed && charge <= 0.0:
+					remove_requested.emit(self)
+				uncharging = event.pressed
